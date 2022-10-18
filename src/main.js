1d4bd1c647c158a7b89d2b109bdaacfb58b4a4b9
MorenaNobre <morenagnobre@gmail.com>
2022-10-18 07:06:28 -0300

    Setting card dinamic logos and colors in javascript with querySelector

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 import "./css/index.css"
+
+//escrito na mão
+const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor1.setAttribute("fill", colors[type][0])
+  ccBgColor2.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+// setCardType("mastercard")
+
+globalThis.setCardType = setCardType
+
+//=============================================================================
+
+//copiado pelo dev tools
+// const ccBgColor1 = document.querySelector(
+//   "#app > section > div.cc-bg > svg > g > g:nth-child(1) > path"
+// )
+// console.log(ccBgColor1)
