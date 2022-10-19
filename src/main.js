27e76f63b207723708cd940177114f09b7a45231
MorenaNobre <morenagnobre@gmail.com>
2022-10-19 13:06:34 -0300

    Added two more credit cards | American Express and Discover

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import Imask from "imask"
 
 //escrito na mão
 const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
@@ -9,6 +10,8 @@ function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
+    americanExpress: ["#563C86", "#22C56D"],
+    discover: ["#FF87C0", "#4874CB"],
     default: ["black", "gray"],
   }
 
@@ -17,9 +20,81 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-// setCardType("mastercard")
+setCardType("discover")
 
 globalThis.setCardType = setCardType
+
+//security code
+const securityCode = document.querySelector("#security-code")
+
+//criar padrão da mascara
+const securityCodePattern = {
+  mask: "0000",
+}
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+//Expiration date
+const expirationDate = document.querySelector("#expiration-date")
+const experitionDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+      maxLength: 2,
+    },
+  },
+}
+
+const expirationDateMasked = IMask(expirationDate, experitionDatePattern)
+
+//Card number for card flag - regex expression
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^3[47]\d{0,13}/,
+      cardtype: "american express",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardtype: "discover",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    return foundMask
+  },
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
 
 //=============================================================================
 
